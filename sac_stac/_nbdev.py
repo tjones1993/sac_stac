0d1055f6b8266f9932b6654b03c1811c0bec888c
tjones1993 <thomas.mccoy.jones@gmail.com>
2020-11-05 07:57:07 +0000

    nw

--- sac_stac/_nbdev.py
+++ sac_stac/_nbdev.py
@@ -4,15 +4,19 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"sedas_client": "00_utils.ipynb",
          "get_sedas_collections": "00_utils.ipynb",
+         "convert2cog": "00_utils.ipynb",
+         "mosaic2cog": "00_utils.ipynb",
          "s3_create_client": "00_utils.ipynb",
          "gb": "00_utils.ipynb",
          "s3_single_upload": "00_utils.ipynb",
          "get_rel_dir_s3_paths": "00_utils.ipynb",
          "s3_upload_dir": "00_utils.ipynb",
          "s3_list_objects_paths": "00_utils.ipynb",
-         "s3_list_objects": "00_utils.ipynb"}
+         "s3_list_objects": "00_utils.ipynb",
+         "pleiades_find_datasets": "01_pleiades.ipynb"}
 
-modules = ["utils.py"]
+modules = ["utils.py",
+           "tbd.py"]
 
 doc_url = "https://tjones1993.github.io/sac_stac/"
 
