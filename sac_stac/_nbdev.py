9251df8e8e1a49982beaaa5d841996fd59f556c4
tjones1993 <thomas.mccoy.jones@gmail.com>
2020-11-06 15:43:25 +0000

    try CI with reqs

--- sac_stac/_nbdev.py
+++ sac_stac/_nbdev.py
@@ -3,7 +3,10 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"sedas_client": "00_utils.ipynb",
-         "get_sedas_collections": "00_utils.ipynb",
+         "sedas_get_collections": "00_utils.ipynb",
+         "sedas_find_datasets": "00_utils.ipynb",
+         "sedas_download": "00_utils.ipynb",
+         "sedas_extract": "00_utils.ipynb",
          "convert2cog": "00_utils.ipynb",
          "mosaic2cog": "00_utils.ipynb",
          "s3_create_client": "00_utils.ipynb",
@@ -12,11 +15,10 @@ index = {"sedas_client": "00_utils.ipynb",
          "get_rel_dir_s3_paths": "00_utils.ipynb",
          "s3_upload_dir": "00_utils.ipynb",
          "s3_list_objects_paths": "00_utils.ipynb",
-         "s3_list_objects": "00_utils.ipynb",
-         "pleiades_find_datasets": "01_pleiades.ipynb"}
+         "s3_list_objects": "00_utils.ipynb"}
 
 modules = ["utils.py",
-           "tbd.py"]
+           "pleiades.py"]
 
 doc_url = "https://tjones1993.github.io/sac_stac/"
 
